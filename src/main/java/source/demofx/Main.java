363e47c73720b99c850d628a884667170754252d
lonhoangsa <laihoangson42@gmail.com>
2024-06-10 15:54:28 +0700

    fix bug zoom

--- src/main/java/source/demofx/Main.java
+++ src/main/java/source/demofx/Main.java
@@ -8,19 +8,18 @@ import javafx.stage.Stage;
 import org.opencv.core.Core;
 
 public class Main extends Application {
-    private CameraStageController controller;
     public static void main(String[] args) {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         launch(args);
 
     }
-
     @Override
     public void start(Stage primaryStage) throws Exception{
         FXMLLoader loader =new FXMLLoader(getClass().getResource("MainStage.fxml"));
         Parent root = loader.load();
         Scene scene = new Scene(root);
         primaryStage.setResizable(false);
+        CameraStageController controller;
         controller = loader.getController();
         controller.setStage(primaryStage);
         primaryStage.setTitle("Main_Stage");
